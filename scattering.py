2e04d9c1f6c06957aa5e652005ca3bb339b0fd99
Francesco Vascelli <francescovasc@gmail.com>
2022-07-05 17:28:37 +0200

    separated te code in different files

---
+++ scattering.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  5 15:58:08 2022
+
+@author: Francesco Vascelli
+"""
+
+import numpy 
+from numpy import random
+
+def random_walk(xy_vector, n_end, depth, steps):
+    for i in range(steps):
+        p = random.rand()
+    
+        if p<float(1/6):
+            turn_right(xy_vector)
+        if p>=float(1/6) and p<float(1/3):
+              turn_left(xy_vector)
+        if p>=float(1/3):
+            go_forward(xy_vector)
+                          
+        if xy_vector[2]==(depth+1):
+            n_end[2]+=1
+            break
+        if xy_vector[2]==-1:
+            n_end[0]+=1
+            break
+    else:
+        n_end[1]+=1
+    
+
+def turn_left(xy_vector):
+    if xy_vector[2]-xy_vector[0]==1: #+x direction
+    
+        xy_vector[0] += 1
+        xy_vector[3] += 1
+        
+    if xy_vector[2]-xy_vector[0]==-1: #-x direction
+        
+        xy_vector[2] += 1
+        xy_vector[3] -= 1
+        
+    if xy_vector[3]-xy_vector[1]==1: #+y direction
+        xy_vector[1] += 1
+        xy_vector[2] -= 1
+        
+    if xy_vector[3]-xy_vector[1]==-1: #-y direction
+        
+        xy_vector[2] += 1
+        xy_vector[3] += 1   
+        
+   
+def turn_right(xy_vector):
+    if xy_vector[2]-xy_vector[0]==1: #+x direction
+    
+        xy_vector[0] += 1
+        xy_vector[3] -= 1
+        
+    if xy_vector[2]-xy_vector[0]==-1: #-x direction
+        
+        xy_vector[2] += 1
+        xy_vector[3] += 1
+        
+    if xy_vector[3]-xy_vector[1]==1: #+y direction
+        xy_vector[1] += 1
+        xy_vector[2] += 1
+        
+    if xy_vector[3]-xy_vector[1]==-1: #-y direction
+        
+        xy_vector[2] -= 1
+        xy_vector[3] += 1   
+        
+
+def go_forward(xy_vector):
+    if xy_vector[2]-xy_vector[0]==1:  #+x direction
+        
+        xy_vector[0] += 1
+        xy_vector[2] += 1
+        
+    if xy_vector[2]-xy_vector[0]==-1: #-x direction
+        
+        xy_vector[0] -= 1
+        xy_vector[2] -= 1
+        
+    if xy_vector[3]-xy_vector[1]==1: #+y direction
+        
+        xy_vector[1] += 1 
+        xy_vector[3] += 1
+        
+    if xy_vector[3]-xy_vector[1]==-1: #-y direction
+        
+        xy_vector[1] -= 1
+        xy_vector[3] -= 1
+        
--- sim.py
+++ sim.py
@@ -2,16 +2,15 @@
 """
 Created on Mon Jul  4 10:28:34 2022
 
-@author: Francesco
+@author: Francesco Vascelli
 """
 
-import numpy as np
+import scattering
+import numpy 
 from numpy import random
 
-""""
-set initial direction of the neutron
-old and new positions help determine the direction
-"""
+
+#initial conditions 
 old_x=0
 old_y=0
 
@@ -21,67 +20,26 @@ new_y=0
 
 xy_vector = [old_x,old_y,new_x,new_y]
 
-"""
-Create array of possible destination of neutrons
+n_particles=1000
 
-"""
 n_back=0
 n_lost=0
 n_through=0
 
-n_end= [n_back,n_lost,n_through]
-
-d=500
+n_end = [n_back,n_lost,n_through]
 
 
-def random_walk(xy_vector):
-    for i in range(100):
-        p = random.rand()
-    
-      #  if p<float(1/6):
-       #     turn_right(xy_vector)
-       # if p>float(1/6) and p<float(1/3):
-       #       turn_left(xy_vector)
-        if p>float(1/3):
-            go_forward(xy_vector)
-                          
-        if xy_vector[2]==d+1:
-            n_end[2]+=1
-            break
-        if xy_vector[2]==-1:
-            n_end[0]+=1
-            break
-    
+depth=50
 
-def turn_left(xy_vector):
-    return 0
+steps=100
 
-def turn_right(xy_vector):
-    return 0
 
-def go_forward(xy_vector):
-    if xy_vector[2]-xy_vector[0]==1:
-        
-        xy_vector[0] += 1
-        xy_vector[2] += 1
-        
-    if xy_vector[2]-xy_vector[0]==-1:
-        
-        xy_vector[0] -= 1
-        xy_vector[2] -= 1
-        
-    if xy_vector[3]-xy_vector[1]==1:
-        
-        xy_vector[1] += 1 
-        xy_vector[3] += 1
-        
-    if xy_vector[3]-xy_vector[1]==-1:
-        
-        xy_vector[1] -= 1
-        xy_vector[3] -= 1
-        
-    
-random_walk(xy_vector)
+
+
+for j in range(n_particles):   
+    xy_vector = [0,0,1,0]
+    scattering.random_walk(xy_vector, n_end, depth, steps)
+
 
 print(n_end)
     
