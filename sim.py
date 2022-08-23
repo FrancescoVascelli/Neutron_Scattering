1573d4b444d755580428aa01cbd0c3f0c3cce263
Alessandro Divita <divita.alessandro@gmail.com>
2022-08-23 15:44:43 +0200

    This file became the entry point of all project. Contains only the `main` function

--- sim.py
+++ sim.py
@@ -4,64 +4,44 @@ Created on Mon Jul  4 10:28:34 2022
 
 @author: Francesco Vascelli
 """
-
-import scattering
-import numpy as np
-
-import configparser
-
-import tqdm
-from tqdm import trange
 import time
-
-import os
-import sys
-
-config = configparser.ConfigParser()
-config.read(sys.argv[1])
-
-os.makedirs('./data',exist_ok=True)
-os.makedirs('./images',exist_ok=True)
-
-destination0 = config.get('paths','n_back')
-destination1 = config.get('paths','n_lost')
-destination2 = config.get('paths','n_through')
-destination3 = config.get('paths','depth')
+import init
+import utility
+import scattering
+import testing
 
 
-n_particles = int(config.get('input','n_particles'))
-steps = int(config.get('input','steps'))
+def main():
+    """
+    This method is the entry point for all programs
+    """
+    t_start = time.perf_counter()
+    init.setup()
+    params = init.params
+    if params.testing:
+        testing.test_all_suite() # pylint: disable=no-value-for-parameter
+        exit(0)
+
+    if params.graph_only:
+        utility.graph_only(data_path=params.data_path,
+                          output_graph_path=params.output_graph_path, print_graph_console=params.print_graph_console)
+    else:
+        parse_data = scattering.scatter(
+            n_particles=params.n_particles, steps=params.steps, max_depth=params.max_depth)
+        x_depth = parse_data.arr_depth
+        n_back = parse_data.arr_back
+        n_lost = parse_data.arr_lost
+        n_through = parse_data.arr_through
+
+        if params.save_data:
+            utility.save_data(data_path=params.data_path, n_back=n_back,
+                             n_lost=n_lost, n_through=n_through, x_depth=x_depth)
+
+        utility.graph_plot(n_back=n_back, n_lost=n_lost, n_through=n_through,
+                           x_depth=x_depth, output_graph_path=params.output_graph_path, print_graph_console=params.print_graph_console)
+        t_end = time.perf_counter()
+        utility.execution_time_message(message='Total execution time',time_start=t_start,time_end=t_end)
 
 
-n_end = [0,0,0]
-
-
-x_depth=[]
-n_back=[]
-n_lost=[]
-n_through=[]
-
-
-
-
-for depth in trange(2,80):
-    for j in range(n_particles):   
-        scattering.random_walk(n_end, depth, steps)
-    x_depth.append(depth)
-    n_back.append(n_end[0]/n_particles)
-    n_lost.append(n_end[1]/n_particles)
-    n_through.append(n_end[2]/n_particles)
-    n_end = [0,0,0]
-    time.sleep(0.1)
-
-    
-np.save(destination0,n_back)
-np.save(destination1,n_lost)
-np.save(destination2,n_through)
-
-np.save(destination3,x_depth)
-    
-
-
-
-
+if __name__ == '__main__':
+    main()
